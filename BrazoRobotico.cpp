1f00b0353d2b1342b247f371300ae856c6e55ad5
Ivan Tortajada Amezcua <itorame@upv.edu.es>
2025-09-29 18:16:37 +0200

    BrazoRobotico.cpp

---
+++ BrazoRobotico.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+
+class BrazoRobotico
+{
+	private:
+		double x, y, z;
+		bool sujetando;
+	public:
+		BrazoRobotico(double x, double y, double z)
+		{
+			this->x = x;
+			this->y = y;
+			this->z = z;
+		}
+		coger()
+		{
+			sujetando = true;
+		}
+		soltar()
+		{
+			sujetando = false;
+		}
+		mover(double xm, double ym, double zm)
+		{
+			x = xm;
+			y = ym;
+			z = zm;
+		}
