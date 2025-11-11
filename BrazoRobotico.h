8e0640e181ade35a4b71a2ac065e6037e5065dbd
Iván Tortajada Amézcua <itorame@upv.edu.es>
2025-11-11 20:47:43 +0100

    Add BrazoRobotico class with basic functionality

--- BrazoRobotico.h
+++ BrazoRobotico.h
@@ -1,1 +1,17 @@
+class BrazoRobotico {
+private:
+    double x, y, z;
+    bool agarrando;
+public:
+    BrazoRobotico(double x=0, double y=0, double z=0);
 
+    double getX() const;
+    double getY() const;
+    double getZ() const;
+    bool estaAgarrando() const;
+    
+    void mover(double nuevoX, double nuevoY, double nuevoZ);
+    void coger();
+    void soltar();
+
+};
