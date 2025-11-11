9c9e708cebc1467d7beb2ba3a5c9671513cec91a
Iván Tortajada Amézcua <itorame@upv.edu.es>
2025-11-11 21:08:21 +0100

    Update BrazoRobotico.cpp

--- BrazoRobotico.cpp
+++ BrazoRobotico.cpp
@@ -1,29 +1,55 @@
-using namespace std;
+#include "BrazoRobotico.h"
+#include <iostream>
 
-class BrazoRobotico
-{
-	private:
-		double x, y, z;
-		bool sujetando;
-	public:
-		BrazoRobotico(double x, double y, double z)
-		{
-			this->x = x;
-			this->y = y;
-			this->z = z;
-		}
-		coger()
-		{
-			sujetando = true;
-		}
-		soltar()
-		{
-			sujetando = false;
-		}
-		mover(double xm, double ym, double zm)
-		{
-			x = xm;
-			y = ym;
-			z = zm;
-		}
+BrazoRobotico::BrazoRobotico(double x, double y, double z){
+	this->x = x;
+	this->y = y;
+	this->z = z;
+	agarrando = false;
+}
+
+double BrazoRobotico::getX() const{
+	return x;
+}
+
+double BrazoRobotico::getY() const{
+	return y;
+}
+
+double BrazoRobotico::getZ() const{
+	return z;
+}
+
+bool BrazoRobotico::getAgarrando() const{
+	return agarrando;
+}
+
+void BrazoRobotico::mover(double xm, double ym, double zm){
+	x = xm;
+	y = ym;
+	z = zm;
+}
+
+void BrazoRobotico::coger(){
+	if(agarrando == false)
+	{
+		agarrando = true;
+		std::cout << "Pinza cerrada.\n";
+	}
+	else
+	{
+		std::cout << "La pinza ya está cerrada.\n";
+	}
+}
+
+void BrazoRobotico::soltar(){
+	if(agarrando == true)
+	{
+		agarrando = false;
+		std::cout << "Pinza abierta.\n";
+	}
+	else
+	{
+		std::cout << "La pinza ya está abierta.\n";
+	}
 }
